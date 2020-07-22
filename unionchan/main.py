8b2dda1f4765c8f0f220f11f1ec3fd7c0cb359be
mochida <rahmanhakim2435@gmail.com>
2020-07-23 01:23:20 +0700

    Memperbaiki bug di main file dan menyelesaikan command nyalakan/matikan

--- unionchan/main.py
+++ unionchan/main.py
@@ -12,57 +12,52 @@ model.load('./src/model/model.tfl')
 
 client = commands.Bot(command_prefix='!union ')
 
-
-class UnionChan:
-    def __init__(self):
-        self.is_on = False
-
-    async def on_message_(self, msg):
-
-        author = msg.author
-        content = msg.content
-        channel = msg.channel
-        sender = channel
-        debugchannel = client.get_channel("DEBUG CHANNEL ID")
-
-        results = model.predict([sekantung_kata(content, kata)])
-        results_index = numpy.argmax(results)
-        tag = label[results_index]
-
-        for tg in data["intents"]:
-            if tg['tag'] == tag:
-                responses = tg['responses']
-        if self.is_on:
-            if args == '--debug':
-                if not author.bot and channel.id == debugchannel.id:
-                    sender.send(random.choice(responses))
-
-            elif args == '--start':
-                if not author.bot:
-                    sender.send(random.choice(responses))
-
-    async def nyalakan_(self, ctx):
-        self.is_on = True
-        ctx.send('Menyalakan bot...')
-
-    async def matikan_(self, ctx):
-        self.is_on = False
-        ctx.send('Mematikan bot...')
-
+global is_on
+is_on = False
 
 @client.event
 async def on_message(msg):
-    await UnionChan().on_message_(msg)
+    author = msg.author
+    content = msg.content
+    channel = msg.channel
+    sender = channel
+    debugchannel = client.get_channel("debug channel id")
 
-# Masih dalam pengembangan
+    results = model.predict([sekantung_kata(content, kata)])
+    results_index = numpy.argmax(results)
+    tag = label[results_index]
+
+    for tg in data["intents"]:
+        if tg['tag'] == tag:
+            responses = tg['responses']
+
+    if "!union" in content:
+        await client.process_commands(msg)
+
+    else:
+        if is_on:
+            if args == '--debug':
+                if not author.bot and channel.id == debugchannel.id:
+                    await sender.send(random.choice(responses))
+
+            elif args == '--start':
+                if not author.bot:
+                    await sender.send(random.choice(responses))
+        else:
+            pass
+
+
 @client.command()
 async def nyalakan(ctx):
-    await UnionChan().nyalakan_(ctx)
+    global is_on
+    is_on = True
+    await ctx.send("Menyalakan bot...")
 
-# Masih dalam pengembangan
 @client.command()
 async def matikan(ctx):
-    await UnionChan().matikan_(ctx)
+    global is_on
+    is_on = False
+    await ctx.send("Mematikan bot...")
 
 
 client.run('TOKEN')
